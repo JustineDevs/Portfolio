10b4fdb8519b205080fde3275bdc04c37a1dbebe
JustineDevs <TraderGOfficial@gmail.com>
2025-12-10 05:53:32 +0800

    chore: update public

---
+++ public/Logo/hyperhack/rename_hyperhack_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Rename HyperHack files to follow naming conventions
+
+mv "HyperHack - Black + White.png" "hyperhack-black-white.png"
+mv "HyperHack - Black + White.svg" "hyperhack-black-white.svg"
+mv "HyperHack - Blue + White.png" "hyperhack-blue-white.png"
+mv "HyperHack - Blue + White.svg" "hyperhack-blue-white.svg"
+mv "HyperHack - Full Black.png" "hyperhack-full-black.png"
+
+echo "HyperHack files renamed successfully!"
