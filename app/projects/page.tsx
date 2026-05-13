8be48c9d372063ba56918aa34620b731012e0109
JustineDevs <TraderGOfficial@gmail.com>
2026-05-13 17:38:03 +0800

    update: modify page.tsx in projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,14 +1,15 @@
-"use client"
-
 import PageLayout from '@/components/layouts/PageLayout'
 import ProjectsHero from '@/components/sections/projects/ProjectsHero'
 import ProjectsGrid from '@/components/sections/projects/ProjectsGrid'
+import { getPublishedProjects } from '@/lib/content/public'
+import { getPublicLegalLinks } from '@/lib/legal-links'
 
-export default function ProjectsPage() {
+export default async function ProjectsPage() {
+  const [projects, legalLinks] = await Promise.all([getPublishedProjects(), getPublicLegalLinks()])
   return (
-    <PageLayout>
+    <PageLayout legalLinks={legalLinks}>
       <ProjectsHero />
-      <ProjectsGrid />
+      <ProjectsGrid projects={projects} />
     </PageLayout>
   )
 }
