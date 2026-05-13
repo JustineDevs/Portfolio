983afdcff7af0fe0c7bbb5bc582e421034bd1478
JustineDevs <TraderGOfficial@gmail.com>
2026-05-13 17:57:30 +0800

    update: modify ResumeContentSection.tsx in professional

--- components/sections/professional/ResumeContentSection.tsx
+++ components/sections/professional/ResumeContentSection.tsx
@@ -30,7 +30,7 @@ export default function ResumeContentSection() {
       role: 'Web3 Community Moderator',
       period: '2023-2026',
       description: 'Managing and engaging with Web3 communities, providing technical support, and fostering community growth.',
-      website: 'https://oneproject.one',
+      website: 'https://projectonepercent.io/',
     },
   ]
 
