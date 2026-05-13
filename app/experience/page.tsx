f09afe674fd1368f77748befb6e3a2f6cfd2f5ce
JustineDevs <TraderGOfficial@gmail.com>
2026-05-13 17:38:02 +0800

    update: modify page.tsx in experience

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,45 +1,13 @@
-"use client"
+import dynamic from 'next/dynamic'
 
-import PageLayout from '@/components/layouts/PageLayout'
-import HeroTitle from '@/components/sections/experience/HeroTitle'
-import TimelineNavigationSection from '@/components/sections/experience/TimelineNavigationSection'
-import ActivitySection from '@/components/sections/ActivitySection'
-import GalleryTestimonials from '@/components/sections/experience/GalleryTestimonials'
-import PixeledCursorTrail from '@/components/ui/PixeledCursorTrail'
+import { getPublicLegalLinks } from '@/lib/legal-links'
 
-export default function ExperiencePage() {
-  return (
-    <>
-      {/* Global Cursor Trail for Experience Page */}
-      <PixeledCursorTrail
-        pixelCount={20}
-        pixelSize={4}
-        shape="square"
-        color="#424242"
-        blur={false}
-        fadeOut={true}
-        progressiveScale={true}
-        trailStyle="solid"
-        animationPreset="fade"
-        spacing={8}
-        stiffness={100}
-        damping={10}
-        trailDuration={0.3}
-      />
-      
-      <PageLayout>
-        {/* Section 1: Hero Title */}
-        <HeroTitle />
-        
-        {/* Section 2: Timeline Navigation & Card */}
-        <TimelineNavigationSection />
-        
-        {/* Section 3: Activity (Heatmap) */}
-        <ActivitySection />
-        
-        {/* Section 4: Gallery & Testimonials */}
-        <GalleryTestimonials />
-      </PageLayout>
-    </>
-  )
+const ExperiencePageClient = dynamic(() => import('@/components/ExperiencePageClient'), {
+  ssr: false,
+  loading: () => <div className="min-h-screen bg-[#F8F8F8]" />,
+})
+
+export default async function ExperiencePage() {
+  const legalLinks = await getPublicLegalLinks()
+  return <ExperiencePageClient legalLinks={legalLinks} />
 }
