ef1f01ca2af21cce46f497bc18878884f8969c67
JustineDevs <TraderGOfficial@gmail.com>
2026-05-13 18:39:25 +0800

    update: modify page.tsx in experience

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,11 +1,5 @@
-import dynamic from 'next/dynamic'
-
 import { getPublicLegalLinks } from '@/lib/legal-links'
-
-const ExperiencePageClient = dynamic(() => import('@/components/ExperiencePageClient'), {
-  ssr: false,
-  loading: () => <div className="min-h-screen bg-[#F8F8F8]" />,
-})
+import ExperiencePageClient from '@/components/ExperiencePageClient'
 
 export default async function ExperiencePage() {
   const legalLinks = await getPublicLegalLinks()
